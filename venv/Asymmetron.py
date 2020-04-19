02103315c48d826dd464eec4a06c3d81a17eab09
Ariathor <Ariathor@users.noreply.github.com>
2020-04-19 18:09:20 +0200

    test

--- venv/Asymmetron.py
+++ venv/Asymmetron.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     # """
     parser = argparse.ArgumentParser()
     parser.parse_args()
+    print(test)
 
     # if sys.argv[1] == "FUN1":
     #     # Can modify later to allow for multiple paths
