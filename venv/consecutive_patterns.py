885a2b5b343c26893e51464206d999332725605a
Ariathor <Ariathor@users.noreply.github.com>
2020-04-27 22:22:39 +0200

    Added parsing for optional argument names. Minor changes to help test

--- venv/consecutive_patterns.py
+++ venv/consecutive_patterns.py
@@ -16,6 +16,17 @@ def path_checker(paths):
             raise IOError(err)
     return pathsL
 
+def name_splitter(names):
+    """
+    This function splits the optional argument names into a list of the different names
+
+    :param names: Input given by the user as a string of names, corresponding to the paths in the path argument, comma seperated
+    :return: a list of all names given by the user (a list with one element if only one path and name was given
+    """
+    namesL = [x.strip() for x in names.split(',')]
+    return names
+
+
 def fun1():
     return
 
@@ -24,15 +35,15 @@ if __name__ == "__main__":
     # Note: Optional arguments have a - or -- in front of them
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="Enter the path of the file to analyze. Can enter multiple paths as a comma separated string, e.g. \"path1, path2\"")
-    parser.add_argument("-n", "--names", help="The name of each of the motif files")
+    parser.add_argument("-n", "--names", help="Optional argument. A name for each of the motif files for more human-readable output. Each name must correspond to a path")
     parser.add_argument("-min", "--min_distance", help="Two consecutive motifs with distance lower than the min_distance will not be considered as significant for the purpose of this analysis. Default = 0", type=int)
     parser.add_argument("-max", "--max_distance", help="Two consecutive motifs with distance higher than the max_distance will not be considered as significant for the purpose of this analysis. Default = infinite", type=int)
     parser.add_argument("-b", "--bins", help="Optional argument. Split output data and graphs in the specified number of bins. Default = 1", type=int)  # Needs rephrasing
     parser.add_argument("-p", "--plots", help="Optional flag. Display output plots", action="store_true")
     # parser.add_argument("-dc", "--expected_div_conv", help="Optional argument. Expected divergence convergence. Default = 0.5", type=float) # Not sure if necessary
     # parser.add_argument("-sp", "--expected_same_op", help="Optional argument. Expected ................ Default = 0.5", type=float)
-    parser.add_argument("-o", "--orientation", help="Optional argument. Orient file(s) relative to annotated BED-formated file(s) and perform the analysis for the un-annotated file with the new annotations.")
-    parser.add_argument("-t", "--threshold", help="Optional argument. Threshold of p-value of consecutive patterns to save in new BED file.")
+    parser.add_argument("-o", "--orientation", help="Optional argument. Orient file(s) relative to annotated BED-formated file(s) and perform the analysis for the un-annotated file with the new annotations. Can enter multiple paths as a comma separated string, e.g. \"path1, path2\"")
+    parser.add_argument("-t", "--threshold", help="Optional argument. Threshold of p-value of consecutive patterns to save in new BED file.", type=int)
     args = parser.parse_args()
     paths = path_checker(args.path)  # Converts the input to a list of paths. List can include only one element, if one path is given by the user
     # Converts the input to a list of path for the orientation files. If this optional argument was not given, the variable is set to None
@@ -40,6 +51,12 @@ if __name__ == "__main__":
         orientation_paths = path_checker(args.orientation)
     except AttributeError:
         orientation_paths = None
+    # Converts the input to a list of names. If this optional argument was not given, the variable is set to none
+    try:
+        names = name_splitter(args.names)
+    except AttributeError:
+        names = None
+
 
 
 
