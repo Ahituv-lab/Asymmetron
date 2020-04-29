f5b2a20347b621029660d0cc972d0322792ff0ce
Ilias Georgakopoulos-Soares <igs@sanger.ac.uk>
2020-04-29 20:18:02 +0100

    changes in oientatio.py

--- venv/orientation.py
+++ venv/orientation.py
@@ -4,8 +4,18 @@ import argparse
 import wrapper_functions as wf
 
 def fun4(args):
+    """
+    This function generates the annotated file and saves it as an output"
+    """
     motif_no_annotation = wf.path_checker(args.motif_no_annotation)
     motif_annotation = wf.path_checker(args.motif.annotation)
+    Annotation_data=strand_annotate_third_BED_overlap(motif_no_annotation,motif_annotation)
+    datafile=open(output,"w")
+    for i in Annotation_data:
+        datafile.write('\t'.join([str(x) for x in i])+'\n')
+    datafile.close()
+    return
+
 
 if __name__ == "__main":
     parser.add_argument("motif_no_annotation", help="BED-formatted file")
