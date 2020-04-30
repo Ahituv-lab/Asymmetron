4feca8f76c0179a258395dbf66e68274c9dc543c
Ilias Georgakopoulos-Soares <igs@sanger.ac.uk>
2020-04-30 02:31:50 +0100

    removed consecutive_asymmetries.py since its the same with contained_asymmetries.py and different from consecutive_patterns.py

--- venv/contained_asymmetries.py
+++ venv/contained_asymmetries.py
@@ -6,23 +6,35 @@ import wrapper_functions as wf
 def fun2(args):
 
     paths, orientation_paths, names = wf.sanitize (args.path, args.orientation, args.names)
+    # this is used for Bonferoni correction
     number_of_files= len(motifs)*len(regions)
 
+    # Folder to save all outputs
     directory = "outputs_contained_asymmetries"
     if not os.path.exists(directory):
         os.makedirs(directory)
-  
+ 
+    # All possible pairs between region files and motif files
     motif_region_pairs,names_pairs=functions.pairs_generator(pathL1,pathL2,NamesL1,NamesL2)
 
+    # Save the results for the final table
     p_pL=[];m_mL=[];p_mL=[];m_pL=[];same_strandL=[];opposite_strandL=[];convergentL=[];divergentL=[];
-    p_valsL=[];p_vals_BonferoniL=[];RatiosL=[];p_val_conv_divergL=[];p_val_conv_diver_BonferoniL=[];Ratio_conv_divergL=[];
+    p_val_same_oppositeL=[];p_val_same_opposite_BonferoniL=[];Ratio_same_oppositeL=[];p_val_conv_divergL=[];p_val_conv_diver_BonferoniL=[];Ratio_conv_divergL=[];
+
+    # Perform all comparisons of each pair
     for i in range(len(motif_region_pairs)): 
 
         p_p,m_m,p_m,m_p,same_strand,opposite_strand,convergent,divergent=functions.overlap(motifs,regions)
-        p_pL.apend(p_p);m_mL.append(m_m);p_mL.append(p_m);m_pL.append(m_pL);
+
+        p_pL.apend(p_p);m_mL.append(m_m); # Same strand orientation
+        p_mL.append(p_m);m_pL.append(m_pL); # Opposite strand orientation
+
         same_strandL.append(same_strand);opposite_strandL.append(opposite_strand);convergentL.append(convergent);divergentL.append(divergent);
+
         #same vs opposite analysis
         Ratio_same_opposite,p_val_same_opposite,p_val_same_opposite_Bonferoni=functions.statistical_evaluation(same_strand,opposite_strand,number_of_files,expected_asym=expected_asym)
+        Ratio_same_oppositeL.append(Ratio_same_opposite);p_val_same_oppositeL.append(p_val_same_opposite);p_val_same_opposite_BonferoniL.append(p_val_same_opposite_Bonferoni)
+
         # convergent vs divergent analysis
         Ratio_conv_diverg,p_val_conv_diverg,p_val_conv_diver_Bonferoni=functions.statistical_evaluation(p_m,m_p,number_of_files,expected_asym=expected_asym_conv_div)
         p_val_conv_divergL.append(p_val_conv_diverg);p_val_conv_divergL.append(p_val_conv_diver_Bonferoni);Ratio_conv_divergL.append(Ratio_conv_diverg);
@@ -32,6 +44,12 @@ def fun2(args):
             functions.barplot_gen(same_strand,opposite_strand,os.path.join(directory, names_pairs[0]+"_"+names_pairs[1]+ "_same_opposite_orientations.png"))
             # generates historam covergent divergent, we need to decide the output2
             functions.barplot_gen(p_m,m_p,os.path.join(directory, names_pairs[0]+"_"+names_pairs[1]+ "_divergent_convergent_orientations.png"))
+
+        if score:
+            pass
+
+        if bins:
+            pass
 
     # generates table <- this should be done for all pairs.
     functions.table_gen(names_pairs,p_pL,m_mL,p_mL,m_pL,p_valsL,p_vals_BonferoniL,RatiosL,p_val_conv_divergL,p_val_conv_diver_BonferoniL,Ratio_conv_divergL)
