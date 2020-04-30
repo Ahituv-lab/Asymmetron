1be53c00ffc3cf851f69f8ff9a7806bac8c83990
Ilias Georgakopoulos-Soares <igs@sanger.ac.uk>
2020-04-30 11:51:22 +0100

    Changes in fun1 and fun3

--- venv/consecutive_patterns.py
+++ venv/consecutive_patterns.py
@@ -1,12 +1,13 @@
 import sys
-#import functions
+import functions
 import argparse
 import wrapper_functions as wf
 
 
 def fun1(args):
     paths, orientation_paths, names = wf.sanitize (args.path, args.orientation, args.names)
-    #functions.asymmetries_single(path,window_min,window_max,bins=0)
+    for path in paths:
+        asymmetries_single(path,min_distance,max_distance,patterns,bins=0)
     return
 
 
@@ -15,6 +16,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="Enter the path of the file to analyze. Can enter multiple paths as a comma separated string, e.g. \"path1, path2\"")
     parser.add_argument("-n", "--names", help="Optional argument. A name for each of the motif files for more human-readable output. Each name must correspond to a path")	   # --patterns eg.. ++/+-+
+    parser.add_argument("-pt","--patterns", help = "Patterns to search, comma separated. Default is ++,--,+-,-+.")
     parser.add_argument("-min", "--min_distance", help="Two consecutive motifs with distance lower than the min_distance will not be considered as significant for the purpose of this analysis. Default = 0", type=int)
     parser.add_argument("-max", "--max_distance", help="Two consecutive motifs with distance higher than the max_distance will not be considered as significant for the purpose of this analysis. Default = 100", type=int)
     parser.add_argument("-b", "--bins", help="Optional argument. Split output data and graphs in the specified number of bins. Default = 1", type=int)  # Needs rephrasing
