33aaa892418f732e37009e8719c1e16205656ddf
Ilias Georgakopoulos-Soares <igs@sanger.ac.uk>
2020-05-07 10:30:53 +0100

    Changes in fun1

--- venv/functions.py
+++ venv/functions.py
@@ -408,7 +408,6 @@ def extract_pattern(DataL, pattern, min_distance, max_distance, threshold):
     consecutiveL= defaultdict(int);
     for i in range(1, len(occs)):
         index = occs[i]
-        print(index+n)
         distance = max(0, int(DataL[index][1]) - int(DataL[index -1][2]))
         if occs[i]-occs[i-1] == n and (distance >= min_distance and distance <= max_distance):
             counter += 1
