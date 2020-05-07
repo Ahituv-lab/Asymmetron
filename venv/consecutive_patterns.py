254fdacb2ac753922e76a104fa3fb14ea4c9e85a
Ilias Georgakopoulos-Soares <igs@sanger.ac.uk>
2020-05-07 16:13:53 +0100

    Minor changes in fun2, starting to add heatmap in fun1

--- venv/consecutive_patterns.py
+++ venv/consecutive_patterns.py
@@ -101,7 +101,8 @@ def fun1(args):
                      # Plot barplot of occs consecutive in each bin
                      visualizations.barplot_single_gen(OccsL,OccsL,"Occurrences",wf.output_path("consecutive_patterns", "png", 'distribution_distances'))
 
-                # Need to add here vizualization as heatmap for all patterns and number of consecutive
+        # Need to add here vizualization as heatmap for all patterns and number of consecutive
+        #ConsecutiveD_Total[i]
 
         # Table with all the outputs for all strands
         functions.table_consecutive(ConsecutiveD_Total,patterns,wf.output_path("consecutive_patterns","txt",path.split("/")[-1],"_Consecutive_Patterns_Total"))
