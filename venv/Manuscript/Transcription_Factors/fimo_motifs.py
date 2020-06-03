627a55248c2ed44a1cf643115b23c0b598584e20
Ilias Georgakopoulos-Soares <igs@sanger.ac.uk>
2020-06-03 22:22:09 +0100

    Adding steps to replicate our analysis

---
+++ venv/Manuscript/Transcription_Factors/fimo_motifs.py
@@ -0,0 +1,6 @@
+import re,os,sys,glob
+
+files=glob.glob("MEME_files_non_redundant/*.meme")
+motif = files[int(sys.argv[1])-1]
+os.system("mkdir outs3/"+motif.split("/")[-1])
+os.system("~/meme/bin/fimo -oc outs3/"+motif.split("/")[-1]+"/ --bfile background_model.hg38 --thresh 1e-4 --max-stored-scores 10000000 "+motif +" hg38.fa")
