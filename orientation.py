585f36c84bdca33d221df4099ab4a3942dd293d9
Ariathor <Ariathor@users.noreply.github.com>
2020-06-13 22:03:00 +0200

    orientation.py fun4() converted to single argument instead of list of paths

--- orientation.py
+++ orientation.py
@@ -7,13 +7,11 @@ def fun4(motif_no_annotation, motif_annotation):
     """
     This function generates the annotated file and saves it as an output"
     """
-    for path in motif_no_annotation:
-        for path2 in motif_annotation:
-            Annotation_data=functions.strand_annotate_third_BED_overlap(path,path2)
-            path_out = wf.output_path("orientation", "bed", os.path.basename(path), os.path.basename(path2))
-            with open(path_out, "w") as f:
-                for i in Annotation_data:
-                    f.write('\t'.join([str(x) for x in i])+'\n')
+    Annotation_data=functions.strand_annotate_third_BED_overlap(motif_no_annotation,motif_annotation)
+    path_out = wf.output_path("orientation", "bed", os.path.basename(motif_no_annotation), os.path.basename(motif_annotation))
+    with open(path_out, "w") as f:
+        for i in Annotation_data:
+            f.write('\t'.join([str(x) for x in i])+'\n')
     return path_out
 
 
@@ -28,7 +26,10 @@ def orientation_wrapper(args):
     """Wrapper calling fun4 with the appropriate arguments"""
     motif_no_annotation = wf.path_checker(args.motif_no_annotation)
     motif_annotation = wf.path_checker(args.motif_annotation)
-    fun4(motif_no_annotation=motif_no_annotation, motif_annotation=motif_annotation)
+    for path1 in motif_no_annotation:
+        for path2 in motif_annotation:
+            fun4(path1, path2)
+
 
 if __name__ == "__main__":
     args = orientation_parser()
