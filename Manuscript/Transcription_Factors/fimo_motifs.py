32bfa864e7b5e9347bf452aa02e1f30d687e816d
Ilias Georgakopoulos-Soares <igs@sanger.ac.uk>
2020-06-16 16:27:09 +0100

    fixed old script

--- Manuscript/Transcription_Factors/fimo_motifs.py
+++ Manuscript/Transcription_Factors/fimo_motifs.py
@@ -8,4 +8,4 @@ motif = files[int(sys.argv[1])-1]
 os.system("mkdir outs/"+motif.split("/")[-1])
 
 # Performs FIMO motif finding
-os.system("~/meme/bin/fimo -oc outs/"+motif.split("/")[-1]+"/ --bfile background_model.hg38 --thresh 1e-4 --max-stored-scores 10000000 "+motif +" hg38.fa")
+os.system("~/meme/bin/fimo -oc outs/"+motif.split("/")[-1]+"/ --bfile background_model.hg38 --thresh 1e-6 --max-stored-scores 10000000 "+motif +" hg38.fa")
