ed4679f9b40f2eb82994e30864655dfd4febee53
Ariathor <Ariathor@users.noreply.github.com>
2020-10-07 21:25:47 +0200

    Fix for extract_pattern

--- functions.py
+++ functions.py
@@ -522,7 +522,7 @@ def extract_pattern(DataL, pattern, min_distance, max_distance, threshold):
         index = occs[i]
         for j in range(n - 1):
             distance = max(0, int(DataL[index + j + 1][1]) - int(DataL[index + j][2]))
-            if distance <= min_distance or distance >= max_distance:
+            if distance < min_distance or distance > max_distance:
                 occs[i] = None
                 break
     occs = [x for x in occs if x is not None]
